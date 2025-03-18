00783ebf1f5e4e57f01f7fb99fed51078380ab64
Mayank Kumar Sharma <mayankfhacker@gmail.com>
2025-03-18 22:57:35 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import { Navbar } from "./components/Navbar";
+import "./App.css";
+import HeroSection from "./components/HeroSection";
+
+const App = () => {
+  return <>
+  <Navbar/>
+  <HeroSection/>
+  </>
+}
+
+export default App;
